4ebb6df4b51a6f9e67e5b49418add89b245991d8
coolfin <sorlti6952@gmail.com>
2024-01-15 17:37:52 +0900

    FECH:set localStorage

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const btn = document.getElementById('todo-btn')
 let todo = document.getElementById('todo')
+ 
+function addTodo(text) {
+  let arr = JSON.parse(window.localStorage.getItem('todo')) || []
+  if(todo.value.trim() !== '') {
+    arr.push({
+      'todo' : text
+    })
+    window.localStorage.setItem('todo', JSON.stringify(arr))
+    
+    todo.value = ''
+    alert(text)
+  }
+}
 
 btn.addEventListener('click', function() {
-  alert(todo.value)
+  addTodo(todo.value)
 })
 
 todo.addEventListener('keydown', function(e) {
   if (e.key === 'enter') {
-    alert(todo.value)
+    addTodo(todo.value)
   }
 })
+
+// let params = {
+//   "id" : id,
+//   "importance" : importance,
+//   "todo" : todo.value, 
+// }
