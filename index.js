2f208a90e868b1be69ef1c6779d8e271e13f4a8d
8ark-dev <sorlti6952@gmail.com>
2024-01-16 22:01:11 +0900

    FECH:add importance

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const btn = document.getElementById('todo-btn')
-let todo = document.getElementById('todo')
- 
-function addTodo(text) {
-  let arr = JSON.parse(window.localStorage.getItem('todo')) || []
-  if(todo.value.trim() !== '') {
-    arr.push({
-      'todo' : text
-    })
-    window.localStorage.setItem('todo', JSON.stringify(arr))
-    
-    todo.value = ''
-    alert(text)
+const btn = document.getElementById('todo-btn');
+const select = document.getElementById('todo-select');
+let todo = document.getElementById('todo');
+
+function addTodo(text, selected) {
+  let arr = JSON.parse(window.localStorage.getItem('todo')) || [];
+
+  if (todo.value.trim() !== '') {
+    let d = {
+      todo: text,
+      importance: selected,
+      done: false,
+    };
+    arr.push(d);
+    window.localStorage.setItem('todo', JSON.stringify(arr));
+
+    alert(
+      '할 일이 추가되었습니다.\n\n' + '중요도: ' + selected + '\n할 일: ' + text
+    );
+
+    todo.value = '';
+    select.value = 1;
+  } else {
+    alert('할 일을 입력해 주세요.');
   }
 }
 
-btn.addEventListener('click', function() {
-  addTodo(todo.value)
-})
+btn.addEventListener('click', function () {
+  addTodo(todo.value, select.children[select.selectedIndex].text);
+});
 
-todo.addEventListener('keydown', function(e) {
-  if (e.key === 'enter') {
-    addTodo(todo.value)
+todo.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    addTodo(todo.value, select.children[select.selectedIndex].text);
   }
-})
-
-// let params = {
-//   "id" : id,
-//   "importance" : importance,
-//   "todo" : todo.value, 
-// }
+});
