33b50ec1eb3aec109d019997292e277a31d338b2
8ark-dev <sorlti6952@gmail.com>
2024-01-19 15:07:55 +0900

    FECH:category

--- todo.js
+++ todo.js
@@ -1,12 +1,13 @@
 const container = document.getElementById('todo-main');
 const todoList = JSON.parse(window.localStorage.getItem('todo'));
 
-document.addEventListener('DOMContentLoaded', function () {
+function loadData(filter) {
   if (todoList) {
     todoSort(todoList);
-
+    container.innerHTML = '';
     for (let i = 0; i < todoList.length; i++) {
-      container.innerHTML += `
+      if (filter === 'all') {
+        container.innerHTML += `
         <div class="todo-item ${
           todoList[i].importance[0] === '0' ? 'color-done' : ''
         }">
@@ -25,12 +26,41 @@ document.addEventListener('DOMContentLoaded', function () {
             <div class="todo-done ${
               todoList[i].importance[0] === '0' ? 'btn-done' : ''
             }" onclick=onClickDone(${i})>${
-        todoList[i].importance[0] === '0' ? '취소' : '완료'
-      }</div>
+          todoList[i].importance[0] === '0' ? '취소' : '완료'
+        }</div>
             <div class="todo-delete" onclick="onClickDelete(${i})">삭제</div>
           </div>
         </div>
       `;
+      } else {
+        if (todoList[i].importance[0] === filter) {
+          container.innerHTML += `
+        <div class="todo-item ${
+          todoList[i].importance[0] === '0' ? 'color-done' : ''
+        }">
+          <div class="todo-item-text">${todoList[i].todo}</div>
+          <div class="todo-item-importance">${
+            todoList[i].importance === '1'
+              ? '낮음'
+              : todoList[i].importance === '2'
+              ? '보통'
+              : todoList[i].importance === '3'
+              ? '높음'
+              : '완료'
+          }</div>
+          <div class="todo-item-date">${todoList[i].date}</div>
+          <div class="todo-item-control">
+            <div class="todo-done ${
+              todoList[i].importance[0] === '0' ? 'btn-done' : ''
+            }" onclick=onClickDone(${i})>${
+            todoList[i].importance[0] === '0' ? '취소' : '완료'
+          }</div>
+            <div class="todo-delete" onclick="onClickDelete(${i})">삭제</div>
+          </div>
+        </div>
+      `;
+        }
+      }
     }
   } else {
     const header = document.getElementById('todo-header');
@@ -38,7 +68,9 @@ document.addEventListener('DOMContentLoaded', function () {
     header.style.display = 'none';
     main.style.display = 'none';
   }
-});
+}
+
+document.addEventListener('DOMContentLoaded', loadData('all'));
 
 //a,b 값이 0보다 작은경우 a가 먼저옴
 //a,b 값이 0보다 큰경우 b가 먼저옴
@@ -67,6 +99,7 @@ function onClickDone(index) {
   }
 
   window.localStorage.setItem('todo', JSON.stringify(todoList));
+
   location.reload();
 }
 
@@ -78,4 +111,26 @@ function onClickDelete(index) {
 
     location.reload();
   }
+}
+
+function handleChangeCategory(e) {
+  const target = e.target;
+
+  if (target.classList.contains('active')) return;
+  else {
+    const active = document.getElementsByClassName('active');
+    active[0].className = active[0].className.replace('active', '');
+    target.className += ' active';
+  }
+
+  if (target.classList.contains('todo-header-item')) {
+    const cate = target.getAttribute('data-category');
+    loadData(cate);
+  }
+}
+
+function tip() {
+  alert(
+    '✔완료 버튼을 누르면 맨 아래로 내려가며, \n❌취소 버튼을 누르면 원래 위치로 돌아옵니다.'
+  );
 }
